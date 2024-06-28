dd2f51980704b4bc04f483b9accd92f260174da6
Amit Masram <amitmasram831@gmail.com>
2024-06-28 22:38:04 +0530

    add - App logo and name

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF00A8E8);
-  static const Color secondary = Color(0xFFFFA41B);
+  static const Color primary = Color(0xFF00A8E8); // Blue
+  static const Color secondary = Color(0xFFFFA41B); // Orange
   // Add more colors as needed
 }
